c89399249a37b68573c5dcd9f725158f83724e33
Artur Molchanov <artur.molchanov@synesis.ru>
2017-03-16 14:30:01 +0300

    Enforce fsck to run non-interactively
    Use '-y' flag instead of '-p'.

--- pkg/util/mount/mount_linux.go
+++ pkg/util/mount/mount_linux.go
@@ -437,7 +437,7 @@ func (mounter *SafeFormatAndMount) formatAndMount(source string, target string, 
 
 	// Run fsck on the disk to fix repairable issues
 	glog.V(4).Infof("Checking for issues with fsck on disk: %s", source)
-	args := []string{"-a", source}
+	args := []string{"-y", source}
 	out, err := mounter.Exec.Run("fsck", args...)
 	if err != nil {
 		ee, isExitError := err.(utilexec.ExitError)
