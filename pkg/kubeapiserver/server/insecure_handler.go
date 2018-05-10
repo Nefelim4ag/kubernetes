dff429978c4a882ab8ca7456dad24670979bbfed
Mikhail Shaverdo <mikhail.shaverdo@synesis.ru>
2018-05-10 17:26:39 +0300

    Implement apiserver max-requests-per-user-inflight option
    Implement optional per-user simultaneously apiserver requests restriction

--- pkg/kubeapiserver/server/insecure_handler.go
+++ pkg/kubeapiserver/server/insecure_handler.go
@@ -34,6 +34,7 @@ func BuildInsecureHandlerChain(apiHandler http.Handler, c *server.Config) http.H
 	handler = genericapifilters.WithAuthentication(handler, server.InsecureSuperuser{}, nil)
 	handler = genericfilters.WithCORS(handler, c.CorsAllowedOriginList, nil, nil, nil, "true")
 	handler = genericfilters.WithTimeoutForNonLongRunningRequests(handler, c.LongRunningFunc, c.RequestTimeout)
+	handler = genericfilters.WithMaxInFlightPerUserLimit(handler, c.MaxRequestsPerUserInFlight, c.LongRunningFunc)
 	handler = genericfilters.WithMaxInFlightLimit(handler, c.MaxRequestsInFlight, c.MaxMutatingRequestsInFlight, c.LongRunningFunc)
 	handler = genericfilters.WithWaitGroup(handler, c.LongRunningFunc, c.HandlerChainWaitGroup)
 	handler = genericapifilters.WithRequestInfo(handler, server.NewRequestInfoResolver(c))
